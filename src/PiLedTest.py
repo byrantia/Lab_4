9a106c744927b11917a6e0f318f2c711456124d8
Byran <BYRANTHC05.24@ichat.sp.edu.sg>
2025-11-11 16:35:42 +0800

    Lab4

--- src/PiLedTest.py
+++ src/PiLedTest.py
@@ -1,10 +1,8 @@
+import RPi.GPIO as GPIO #import RPi.GPIO module
 import hal.hal_led as led
 import hal.hal_input_switch as switch
 import time
 from time import sleep
-import RPi.GPIO as GPIO #import RPi.GPIO module
-from time import sleep
-
 
 def main():
     # Initialize LED HAL driver 
@@ -15,10 +13,9 @@ def main():
     GPIO.setup(22,GPIO.IN) #set GPIO 22 as input
     start_time = time.time()
     end_time = start_time + 5
-
     #Set LED output level, 1 = ON, 0 = OFF
     while (1):
-        if GPIO.input(22)==0:
+        if switch.read_slide_switch()==0:
             led.set_output(0, 1)
             sleep(0.1)
             led.set_output(0, 0)
@@ -29,7 +26,9 @@ def main():
                 sleep(0.05)
                 led.set_output(0, 0)
                 sleep(0.05)    
-        
+            while switch.read_slide_switch()==1:
+                start_time = time.time()
+                end_time = start_time + 5
 
 
 # Main entry point
